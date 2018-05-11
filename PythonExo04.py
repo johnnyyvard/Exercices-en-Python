af18e7c1dafaddcaa90d51894e38fe44242cea7b
johnnyyvard <36401183+johnnyyvard@users.noreply.github.com>
2018-05-11 18:55:48 +0200

    Update PythonExo04.py
    Écrire un algorithme qui donne la somme de 2 chiffres si l'un est double de l'autre.

--- PythonExo04.py
+++ PythonExo04.py
@@ -1,9 +1,8 @@
-#Ecrire un programme qui affiche la somme de deux nombres si l un est le double de
-#l autre, "rien" sinon.
-
 def afficheCond(a,b):
+	
 	if ((a==2*b)or(b==2*a)):
 		return a+b
 	else:
 		return "rien"
+	
 print(afficheCond(6,3))
