07c9efc99636a1feef9bdbc43a21b3330e8512b9
johnnyyvard <johnny.yvard@imie.fr>
2018-05-14 18:02:52 +0200

    Écrire un programme qui affiche la somme de deux nombres...

---
+++ PythonExo09.py
@@ -0,0 +1,13 @@
+#...si l un est le double de l autre, ou si la variable booleenne 
+#affiche prend la valeur vrai.
+
+def afficheCond(a,b,affiche):
+
+	if ((a==2*b)or(b==2*a)or affiche):
+		return a+b
+	else:
+		return "rien"
+
+print(afficheCond(2,3,True))
+print(afficheCond(2,3,False))
+print(afficheCond(3,6,False))
