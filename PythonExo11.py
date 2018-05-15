cd22a118db149bea290255191b989fe5d57ea19c
johnnyyvard <johnny.yvard@imie.fr>
2018-05-15 10:52:54 +0200

    Écrire un programme qui prend une variable nb de ...

---
+++ PythonExo11.py
@@ -0,0 +1,27 @@
+# ... type integer et qui affiche:
+# - le nombre nb si nb est impair
+# - le nombre nb/2 si nb est pair
+# - le message "attention" si le nombre nb est negatif
+
+def afficheCond(a):
+
+	resultat = ""
+	
+	if a<0:
+	
+
+		resultat = "attention ! "
+
+	if (a%2)==0:
+	
+		resultat += str(a/2)
+	
+	else:
+	
+		resultat += str(a)
+	
+	return(resultat)	
+
+print(afficheCond(-5))
+print(afficheCond(10))
+print(afficheCond(3))
